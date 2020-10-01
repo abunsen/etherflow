7ec54e66e8dac4782207a9805e1bb5e61a157001
Patrick <blockchainbuddha@gmail.com>
2020-10-01 17:17:57 -0400

    bug fix for multiple functions

--- src/components/methodCall.js
+++ src/components/methodCall.js
@@ -51,7 +51,7 @@ const BooleanSelect = ({ initialVal, updateValue }) => {
 const Dropdown = ({ dropdownOptions = [], updateValue, disabled, val }) => {
   return (
     <select
-      value={val}
+      value={val ? val : ''}
       className="my-1 form-select block w-full pl-3 pr-10 py-2 text-base leading-6 border-gray-300 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 sm:text-sm sm:leading-5"
       onChange={(e) => updateValue(e.target.value)}
       disabled={disabled}
